309b4c8ac4584caadd13e253e830a7b8dad0036e
lamhoangsudo <hlam5370@gmail.com>
2023-12-05 15:58:33 +0700

    GRIDMAP
    add gridmap

---
+++ Assets/GridMap/Scripts/Testing.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using System.ComponentModel;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+    [SerializeField]
+    private Transform statPosition;
+    [SerializeField]
+    [DefaultValue(0)]
+    private int width, height, cellSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(width, height, cellSize, new Vector2(statPosition.localPosition.x, statPosition.localPosition.y));
+        grid.TextPoint(grid);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //releases right mouse button
+        if(Input.GetMouseButtonUp(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 20);
+        }
+        //hold press right mouse button
+        if(Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 30);
+        }
+        //releases left mouse button
+        if (Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("last value: " + grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+        //hold press left mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("last value: " + grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+        if (Input.GetMouseButton(2))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 40);
+        }
+        //grid.SetValue(UtilsClass.GetMouseWorldPosition(), 50);
+    }
+}
