d8718ee43f6c7ccce0d8d146a1bb57d6ba08cdce
lamhoangsudo <hlam5370@gmail.com>
2023-12-05 15:43:15 +0700

    GRIDMAP
    ADD_GRIDMAP

--- Assets/GridMap/Scripts/Testing.cs
+++ Assets/GridMap/Scripts/Testing.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
+using System.ComponentModel;
 
 public class Testing : MonoBehaviour
 {
     private Grid grid;
     [SerializeField]
     private Transform statPosition;
+    [SerializeField]
+    [DefaultValue(0)]
+    private int width, height, cellSize;
     // Start is called before the first frame update
     void Start()
     {
-        grid = new Grid(10, 10, 5f, new Vector2(statPosition.localPosition.x, statPosition.localPosition.y));
+        grid = new Grid(width, height, cellSize, new Vector2(statPosition.localPosition.x, statPosition.localPosition.y));
         grid.TextPoint(grid);
     }
 
@@ -42,6 +46,6 @@ public class Testing : MonoBehaviour
         {
             grid.SetValue(UtilsClass.GetMouseWorldPosition(), 40);
         }
-        grid.SetValue(UtilsClass.GetMouseWorldPosition(), 50);
+        //grid.SetValue(UtilsClass.GetMouseWorldPosition(), 50);
     }
 }
